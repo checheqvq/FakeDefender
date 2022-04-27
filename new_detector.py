5db96a76ff71a4f63e9496f5f11e01744fe41ce1
theShuai-t <769674197@qq.com>
2022-04-27 12:22:13 +0800

    添加团队模型

--- new_detector.py
+++ new_detector.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
-from PIL import Image
 from torch import torch
-import torch.nn.functional as F
 from torchvision import transforms as T
 from face_utils import norm_crop, FaceDetector
 from model_def.new_xception import xception
 
 
-class DFDCImageLoader:
+class ImageLoader:
     def __init__(self, face_detector, transform=None):
         self.face_detector = face_detector
         self.transform = transform
@@ -21,15 +19,11 @@ class DFDCImageLoader:
         model = self.model
 
         img = norm_crop(img, landmarks, image_size=299)
-
-
-        # 转换成张量
         if self.transform:
             img = self.transform(img)
 
         batch_buf = []
         batch_buf.append(img)
-        # torch.stack()是将原来的几个tensor按照一定方式进行堆叠，然后在按照堆叠后的维度进行切分。
         batch = torch.stack(batch_buf)
         batch = batch.cpu()
         output = model(batch, batch)
@@ -56,17 +50,9 @@ class DFDCImageLoader:
 
 # load model
 # 只要设置了torch.set_grad_enabled(False)那么接下来所有的tensor运算产生的新的节点都是不可求导的，
-# 这个相当于一个全局的环境，即使是多个循环或者是在函数内设置的调用，
-# 只要torch.set_grad_enabled(False)出现，则不管是在下一个循环里还是在主函数中，都不在求导，
-# 除非单独设置一个孤立节点，并把他的requires_grad设置成true。
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1,2'
 torch.set_grad_enabled(False)
-# 让程序在开始时花费一点额外时间，为整个网络的每个卷积层搜索最适合它的卷积实现算法，进而实现网络的加速
 torch.backends.cudnn.benchmark = True
-# 加载检查点
 face_detector = FaceDetector()
-# RetinaFace-Resnet50-fixed.pth-->Pretrained RetinaFace模型
 face_detector.load_checkpoint("./input/dfdc-pretrained-2/RetinaFace-Resnet50-fixed.pth")
-# T.ToTensor() 把图片转换成张量的形式  print(T.ToTensor()) --> ToTensor()
-loader = DFDCImageLoader(face_detector, T.ToTensor())
+loader = ImageLoader(face_detector, T.ToTensor())
 print("loader ok")
